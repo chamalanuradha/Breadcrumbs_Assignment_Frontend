84fdb595c14132bf46faa5ad7b0f0b6076728bed
chamalanuradha <chamalanuradha98765@gmail.com>
2024-10-04 09:25:26 +0530

    Change login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,15 +1,50 @@
-/* eslint-disable jsx-a11y/img-redundant-alt */
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
+import './Login.css'
 
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-const Login =  () => {
+  const handleLogin = (e) => {
+    e.preventDefault();
 
- 
+  };
+
   return (
-    <div className="landing_body_container">
-<h1>Login</h1>
-    </div>
+    <div className="login-container">
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
 
-  )
-}
-export default Login
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+
+        <button type="submit">Login</button>
+      </form>
+      
+      <p className="redirect-text">
+        Don't have an account? <Link to="/register">Register here</Link>
+      </p>
+    </div>
+  );
+};
+
+export default Login;
