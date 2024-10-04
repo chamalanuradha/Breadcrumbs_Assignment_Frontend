0c63af30146ca1bbce5d8fb563def8be12199c25
chamalanuradha <chamalanuradha98765@gmail.com>
2024-10-04 09:03:43 +0530

    set routes login and register pages

---
+++ src/pages/Login.js
@@ -0,0 +1,15 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
+import React from 'react';
+
+
+const Login =  () => {
+
+ 
+  return (
+    <div className="landing_body_container">
+<h1>Login</h1>
+    </div>
+
+  )
+}
+export default Login
